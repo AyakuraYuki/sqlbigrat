ec8262c5653297d1ea1822726108fe298975a0b0
Ayakura Yuki <YukiAyakura@outlook.com>
2024-07-19 23:13:53 +0800

    rat

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/AyakuraYuki/sqlbigrat
 
 go 1.22.5
-
-require github.com/shopspring/decimal v1.4.0
--- rat.go
+++ rat.go
@@ -1,134 +1,89 @@
 package sqlbigrat
 
 import (
-	"database/sql"
 	"database/sql/driver"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"math/big"
-
-	"github.com/shopspring/decimal"
-)
-
-var (
-	_ sql.Scanner      = (*Rat)(nil)
-	_ driver.Valuer    = (*Rat)(nil)
-	_ json.Marshaler   = (*Rat)(nil)
-	_ json.Unmarshaler = (*Rat)(nil)
 )
 
 type Rat struct {
 	*big.Rat
-	decimal.Decimal
-	Valid bool
+}
+
+func NewFromInt(i int64) *Rat     { return &Rat{new(big.Rat).SetInt64(i)} }
+func NewFromUInt(u uint64) *Rat   { return &Rat{new(big.Rat).SetUint64(u)} }
+func NewFromFloat(f float64) *Rat { return &Rat{new(big.Rat).SetFloat64(f)} }
+
+func NewFromString(s string) (*Rat, error) {
+	r, ok := new(big.Rat).SetString(s)
+	if !ok {
+		return nil, fmt.Errorf("[sqlbigrat.Rat] failed to convert value %q to byte array of type %T", s, s)
+	}
+	return &Rat{r}, nil
 }
 
 func (r *Rat) Scan(src any) error {
 	if rat, ok := src.(*big.Rat); ok {
-		r.Valid = true
 		r.Rat = rat
-		r.Decimal = decimal.NewFromBigRat(r.Rat, 32)
 		return nil
 	}
 
 	switch val := src.(type) {
 	case int:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetInt64(int64(val))
 	case int8:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetInt64(int64(val))
 	case int16:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetInt64(int64(val))
 	case int32:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetInt64(int64(val))
 	case int64:
-		r.Decimal = decimal.NewFromInt(val)
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetInt64(val)
 	case uint:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetUint64(uint64(val))
 	case uint8:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetUint64(uint64(val))
 	case uint16:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetUint64(uint64(val))
 	case uint32:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetUint64(uint64(val))
 	case uint64:
-		r.Decimal = decimal.NewFromInt(int64(val))
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetUint64(val)
 	case float32:
-		r.Decimal = decimal.NewFromFloat32(val)
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetFloat64(float64(val))
 	case float64:
-		r.Decimal = decimal.NewFromFloat(val)
-		r.Rat = r.Decimal.Rat()
-		r.Valid = true
-		return nil
+		r.Rat = new(big.Rat).SetFloat64(val)
 	case string:
-		var ok bool
-		r.Rat, ok = (&big.Rat{}).SetString(val)
+		rat, ok := new(big.Rat).SetString(val)
 		if !ok {
-			return fmt.Errorf("[sqlrat.BqRat] invalid BIGNUMERIC value %+v of type %T", src, src)
+			return fmt.Errorf("[sqlbigrat.Rat] invalid value %+v of type %T", src, src)
 		}
-		r.Valid = true
-		r.Decimal = decimal.NewFromBigRat(r.Rat, 32)
-		return nil
+		r.Rat = rat
 	default:
-		return errors.New(fmt.Sprintf("[sqlrat.BqRat] failed to convert value %+v to byte array of type %T", src, src))
+		return fmt.Errorf("[sqlbigrat.Rat] failed to convert value %+v to byte array of type %T", src, src)
 	}
+
+	return nil
 }
 
 func (r *Rat) Value() (driver.Value, error) {
-	if !r.Valid {
+	if r == nil || r.Rat == nil {
 		return nil, nil
 	}
 	return r.Rat.String(), nil
 }
 
 func (r *Rat) MarshalJSON() ([]byte, error) {
-	return r.Decimal.MarshalJSON()
+	if r.Rat == nil {
+		return []byte("null"), nil
+	}
+	return []byte(r.Rat.FloatString(16)), nil
 }
 
 func (r *Rat) UnmarshalJSON(data []byte) error {
-	dec := decimal.NewFromInt(0)
-	if err := dec.UnmarshalJSON(data); err != nil {
-		return err
-	}
-	r.Decimal = dec
-	r.Rat = dec.Rat()
-	r.Valid = true
-	return nil
+	return r.Scan(string(data))
 }
 
 func (r *Rat) String() string {
-	return r.Decimal.String()
+	return r.Rat.FloatString(16)
 }
